b27487c53212e22f55c63fb035041e702107adea
ZUrufu26 <zaptoswolf@gmail.com>
2024-10-22 01:03:27 +1200

    Telegram-бот с базовым функционалом

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const TelegramBot = require('node-telegram-bot-api');
 // Замените 'YOUR_BOT_TOKEN' на токен вашего бота
 const bot = new TelegramBot('7675869759:AAHeC_nx7UJm5j-lW63orhWxKz9BpprCxD8', { polling: true });
 
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привет! Я ваш новый бот.');
+});
+
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-    const messageText = `Привет, ты написал: ${msg.text}`;
+    const messageText = `Ты написал: ${msg.text}`;
 
-    bot.sendMessage(chatId, messageText);
+    // Обработчик для других сообщений
+    if (msg.text !== '/start') {
+        bot.sendMessage(chatId, messageText);
+    }
 });
 
 console.log('Бот запущен!');
